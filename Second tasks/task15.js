dcd071a654093f41c8adfc42cb0243fd817f8af2
TimmyGOGO <temich941@mail.ru>
2018-12-13 15:53:57 +0300

    Векторный тип
    Задача решена в соответствии со стандартом ES5

---
+++ Second tasks/task15.js
@@ -0,0 +1,38 @@
+//Мой код:
+function Vector(x, y){
+    this.x = x;
+    this.y = y;
+}
+  
+//добавим через prototype:
+//getter:
+Object.defineProperty(Vector.prototype, "length", {
+    get: function() { return Math.sqrt(this.x * this.x+ 
+                                       this.y * this.y); }
+});
+  
+//метод плюс:
+Vector.prototype.plus = function(vec){
+    return new Vector(this.x + vec.x,
+                      this.y + vec.y);
+}
+  
+//метод минус:
+Vector.prototype.minus = function(vec){
+    return new Vector(this.x - vec.x,
+                      this.y - vec.y);
+}
+  
+/*у нас получился "класс" Vector,
+  который прототипно наследует getter и пару методов 
+  от некоторого объекта Vector.prototype 
+*/
+  
+//checking:
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
+  
